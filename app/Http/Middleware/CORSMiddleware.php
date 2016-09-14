ff4aa20e0e36f4f0c2ed24585f44a666c7b6e04b
askalia <joris@askalia.net>
2016-09-14 16:53:07 +0200

    get rid of commented code

--- app/Http/Middleware/CORSMiddleware.php
+++ app/Http/Middleware/CORSMiddleware.php
@@ -15,28 +15,15 @@ class CORSMiddleware
      */
     public function handle($request, Closure $next)
     {
-        /*
-         * $content = $next($request);
-             return ( new Response($content) )->header('Access-Control-Allow-Origin' , '*')
-                                            ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
-                                            ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin');
-
-         */
-        //return $next($request);
-
         $headers = [
-              //'Access-Control-Allow-Origin' => '*',
-              //'Access-Control-Allow-Credentials' => true,
                  'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
-                 //'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin'
-                //'Access-Control-Allow-Headers'=> '*'
+              
              ];
-        //if($request->getMethod() == "OPTIONS")
-        //{
+        if($request->getMethod() == "OPTIONS")
+        {
             // The client-side application can set only headers allowed in Access-Control-Allow-Headers
-            //return Response::make('OK', 200, $headers);
-          //  return new \Illuminate\Http\Response('OK', 200, $headers);
-        //}
+            return Response::make('OK', 200, $headers);
+        }
         $response = $next($request);
         foreach($headers as $key => $value){
               $response->header($key, $value);
